4f27e6211f4c7a0f3badef3636e49332183aa4da
KrissNC <mistralkriss@gmail.com>
2019-09-17 13:35:07 +0200

    Updates

---
+++ precache-manifest.f389c8ff24632c46fa10950819556a5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5221f5f146ab5f06e9d26482f40f3346",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "ad1c3bf62268279f8677",
+    "url": "/monsters-rolodex-complete/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex-complete/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "ad1c3bf62268279f8677",
+    "url": "/monsters-rolodex-complete/static/js/main.ca639e41.chunk.js"
+  },
+  {
+    "revision": "683efa6afd61321d8334",
+    "url": "/monsters-rolodex-complete/static/js/runtime~main.0e367587.js"
+  }
+]);
